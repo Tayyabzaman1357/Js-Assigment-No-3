14d1e4c639aeeb8a8c9d653b764d213cccd7d3b8
Tayyab Zaman <tayyabzaman807@gmail.com>
2024-10-20 07:42:18 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+//                            Assigment    3            //
+
+// Q No 1:
+// Ans : 1;
+ var myAge=15;
+ alert("I am " + myAge +  " Year Old. ");
+
+
+
+
+// Q No 2:
+// Ans : 2:
+ var pageVisit=14;
+ alert("You have visited this site" + pageVisit + " times ");
+
+
+
+// Q No 3:
+// Ans : 3:
+ var birthYear=2009;
+ document.write("My birth year is &nbsp;"+   birthYear + "<br>" + "Data type of my declared variable is number.");
+
+
+
+
+// Q No 4;
+// Ans : 4:
+ var websiteName="www.xyzClothing.com"
+ var visitorName="Joe";
+ var productName="Shirts";
+ var productQty=7;
+ document.write(  "<b>"  +               visitorName  + "</b>" +  " ordered  &nbsp;" + "<b>" + productQty + "</b>"  + "&nbsp" +  "<b>" + productName + "</b>" +"&nbsp"+ "on &nbsp" +  websiteName +"&nbsp"+ "Store." );
